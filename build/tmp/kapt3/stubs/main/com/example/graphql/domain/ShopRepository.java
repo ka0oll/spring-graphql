6d697532af17b4aa9c26326df65f0f7bba395831
ka0oll <jy.jeong8802@gmail.com>
2023-03-20 16:06:17 +0900

    git 설정 수정

--- build/tmp/kapt3/stubs/main/com/example/graphql/domain/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.graphql.domain;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u00012\b\u0012\u0004\u0012\u00020\u00020\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/example/graphql/domain/ShopRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lcom/example/graphql/domain/Shop;", "", "Lorg/springframework/data/querydsl/QuerydslPredicateExecutor;", "graphql"})
-public abstract interface ShopRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.graphql.domain.Shop, java.lang.Long>, org.springframework.data.querydsl.QuerydslPredicateExecutor<com.example.graphql.domain.Shop> {
-}
