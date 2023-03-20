9136a374159548f8e04a7bbbaa36f5c28a1f5ebd
ka0oll <jy.jeong8802@gmail.com>
2023-03-20 16:07:11 +0900

    CustomMaxQueryDepthInstrumentation 추가

--- src/main/kotlin/com/example/graphql/config/GraphQlConfig.kt
+++ src/main/kotlin/com/example/graphql/config/GraphQlConfig.kt
@@ -1,15 +1,17 @@
 package com.example.graphql.config
 
+import graphql.ExecutionResult
 import graphql.analysis.MaxQueryDepthInstrumentation
+import graphql.execution.instrumentation.InstrumentationContext
+import graphql.execution.instrumentation.SimpleInstrumentationContext
+import graphql.execution.instrumentation.parameters.InstrumentationExecuteOperationParameters
 import graphql.schema.Coercing
 import graphql.schema.GraphQLScalarType
-import graphql.schema.idl.RuntimeWiring
 import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.graphql.execution.GraphQlSource.SchemaResourceBuilder
 import java.time.LocalDateTime
-import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 
@@ -20,7 +22,7 @@ class GraphQlConfig {
     fun sourceBuilderCustomizer(): GraphQlSourceBuilderCustomizer? {
         return GraphQlSourceBuilderCustomizer { builder: SchemaResourceBuilder ->
             builder.instrumentation(listOf(
-                MaxQueryDepthInstrumentation(5),
+                CustomMaxQueryDepthInstrumentation(5)
             ))
 
             builder.configureRuntimeWiring {
@@ -28,10 +30,7 @@ class GraphQlConfig {
             }
         }
     }
-
 }
-
-
 
 val dateTime = GraphQLScalarType.newScalar()
     .name("DateTime")
@@ -50,3 +49,19 @@ val dateTime = GraphQLScalarType.newScalar()
         }
     })
     .build()
+
+
+class CustomMaxQueryDepthInstrumentation(maxDepth:Int): MaxQueryDepthInstrumentation(maxDepth){
+
+    override fun beginExecuteOperation( parameters:InstrumentationExecuteOperationParameters):InstrumentationContext<ExecutionResult> {
+        try {
+            if(parameters.executionContext.executionInput.operationName.equals("IntrospectionQuery", true)){
+                return SimpleInstrumentationContext.noOp()
+            }
+        }catch (e:Exception){
+            //nothing
+        }
+
+        return super.beginExecuteOperation(parameters)
+    }
+}
